b5b3c61f99d3de298a04c015c09d3f7e6ec8444f
Alp Oran <alporan@windowslive.com>
2019-08-29 12:46:05 +0300

    Add files via upload

--- troya-gui/funcPrintTsoListing.py
+++ troya-gui/funcPrintTsoListing.py
@@ -11,20 +11,35 @@ def main(t):
     screen_output = t.tso_entry("<GETSCREEN>")
     screen_lines = screen_output.split('\n')
     for i in range(4, len(screen_lines)-1):
-        if not screen_lines[i].isspace():
-            matrix.append([])
-            index_x += 1
-            for j in range(0, len(screen_lines[i])):
-                matrix[index_x].append(screen_lines[i][j])
+        matrix.append([])
+        index_x += 1
+        for j in range(0, len(screen_lines[i])):
+            matrix[index_x].append(screen_lines[i][j])
     while "    Relocation Dictionary   " not in screen_output:
         screen_output = t.tso_entry("DOWN 27")
         screen_lines = screen_output.split('\n')
         for i in range(4, len(screen_lines)-1):
-                if not screen_lines[i].isspace():
-                    matrix.append([])
-                    index_x += 1
-                    for j in range(0, len(screen_lines[i])):
-                        matrix[index_x].append(screen_lines[i][j])
+            matrix.append([])
+            index_x += 1
+            for j in range(0, len(screen_lines[i])):
+                matrix[index_x].append(screen_lines[i][j])
+
+    index_x = -1
+    screen_output = t.tso_entry("TOP")
+    screen_output = t.tso_entry("RIGHT")
+    screen_lines = screen_output.split('\n')
+    for i in range(4, len(screen_lines)-1):
+        index_x += 1
+        for j in range(38, len(screen_lines[i])):
+            matrix[index_x].append(screen_lines[i][j])
+    while "    Relocation Dictionary   " not in screen_output:
+        screen_output = t.tso_entry("DOWN 27")
+        screen_lines = screen_output.split('\n')
+        for i in range(4, len(screen_lines)-1):
+            index_x += 1
+            for j in range(38, len(screen_lines[i])):
+                matrix[index_x].append(screen_lines[i][j])
+
     for i in range(0, len(matrix)):
             for j in range(0, len(matrix[i])):
                 f.write(matrix[i][j])
